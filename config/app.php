94adcd0041229d70aecebb5041ff758d794d122a
Mohammad Salah <sourav.cse5.bu@gmail.com>
2023-12-25 09:50:38 +0600

    chore: debugbar installed

--- config/app.php
+++ config/app.php
@@ -173,6 +173,7 @@ return [
             // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
     ])->toArray(),
 
     /*
